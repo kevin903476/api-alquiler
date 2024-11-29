321ae8651689bcf46300a70e9de91e333f4b21ff
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-11-29 14:21:36 -0600

    cambios ni idea
    hola

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -3,43 +3,24 @@
 class Conectar {
     // Variable protegida para almacenar la instancia de la conexión
     protected $conexion_bd;
-
+    
     // Método protegido para establecer la conexión con la base de datos
     protected function conectar_bd() {
         try {
-            // Datos de conexión a la base de datos
-            $host = "mysql.railway.internal"; 
-            $user = "root";           
-            $pass = "JGMckceHxTYheiHBCakwuvlmiepLyAow";        
-            $db = "railway";   
-            $port = "3306";
-
-            // Establece la conexión utilizando mysqli_connect
-            $this->conexion_bd = mysqli_connect($host, $user, $pass, $db, $port);
-
-            // Verifica si la conexión fue exitosa
-            if (!$this->conexion_bd) {
-                die("Error en la conexión a la base de datos: " . mysqli_connect_error());
-            }
-
-            // Configura la codificación UTF-8
-            mysqli_set_charset($this->conexion_bd, "utf8");
-
-            return $this->conexion_bd;
+            // Establece la conexión utilizando PDO
+            $conexion = $this->conexion_bd = new PDO("mysql://root:JGMckceHxTYheiHBCakwuvlmiepLyAow@mysql.railway.internal:3306/railway);
+            return $conexion;
         } catch (Exception $e) {
             // Si ocurre un error, muestra el mensaje de error y detiene la ejecución
-            die("Error en la base de datos: " . $e->getMessage());
+            print "Error en la base de datos: " . $e->getMessage() . "<br/>";
+            die();  // Detiene la ejecución
         }
     }
 
     // Método público para establecer la codificación de caracteres a UTF-8
     public function establecer_codificacion() {
-        // Verifica si la conexión está inicializada
-        if ($this->conexion_bd) {
-            return $this->conexion_bd->query("SET NAMES 'utf8'");
-        } else {
-            die("No se pudo establecer la codificación: Conexión no inicializada.");
-        }
+        // Ejecuta la sentencia SQL para configurar la codificación de caracteres a UTF-8
+        return $this->conexion_bd->query("SET NAMES 'utf8'");
     }
 }
 ?>
